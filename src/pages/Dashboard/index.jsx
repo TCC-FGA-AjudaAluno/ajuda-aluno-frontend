f25a2a3a184a604781e22029cf35e231eb47f8a4
MatheusFilipe95 <mafieu666@hotmail.com>
2023-10-28 18:49:54 -0300

    Delimitação Home view

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,13 +1,24 @@
-import './Dashboard.module.css'
+import styles from './Dashboard.module.css'
 import Footer from "../../components/Footer"
 import Topbar from '../global/Topbar/Topbar'
+import Chart from '../../components/Chart'
 
 function Dashboard() {
    return (
       <div>
          <Topbar/>
-         <section className='container'>
-            <div className='apresentacao'>
+         <section className={styles.container}>
+            <div className={`${styles.columnContent} ${styles.rect}`}>
+               <Chart/>
+            </div>
+            <div className={`${styles.columnContent} ${styles.rect}`}>
+               <h3>Componente 2</h3>
+            </div>
+            <div className={`${styles.columnContent} ${styles.square}`}>
+               <h3>Componente 3</h3>
+            </div>
+            <div className={`${styles.columnContent} ${styles.square}`}>
+               <h3>Componente 4</h3>
             </div>
          </section>
          <Footer />
