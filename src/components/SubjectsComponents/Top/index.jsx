e9118847dac14f7b892faaa6561e8d1a2438c111
Joao victor <joaovictor-fza@hotmail.com>
2023-12-01 18:08:44 -0300

    fix subject page

--- src/components/SubjectsComponents/Top/index.jsx
+++ src/components/SubjectsComponents/Top/index.jsx
@@ -3,14 +3,14 @@ import styles from './Top.module.css'
 
 function Stats() {
    return (
-      <div style={{display: "grid"}}>
+      <div style={{display: "grid"} }>
          <div className={styles.subject}>
             <p > Requisitos </p>
          </div>
-         <div style={{height: "200px", marginBottom:"25px"}}>
+         <div style={{height: "13px", marginBottom:"25px"}}>
          </div>
-         <div style={{display: "flex", alignItems: "center", marginLeft: "28px"}}>
-            <CalendarMonthOutlinedIcon style={{width: "38px", height: "35px", marginRight: "9px"}}/>
+         <div style={{display: "flex", alignItems: "center", marginLeft: "28px" , height: "25px" }}>
+            <CalendarMonthOutlinedIcon style={{width: "30px", height: "35px", marginRight: "9px"}}/>
             <span>90 dias <br/> Fim do semestre</span>
          </div>
       </div>
--- src/pages/Subjects/index.jsx
+++ src/pages/Subjects/index.jsx
@@ -23,7 +23,7 @@ function Dashboard() {
             </div>
          <section className={styles.container}>
             <div className={`${styles.columnContent} ${styles.rect}`}>
-               <h3>Fga Informações</h3>
+               <h3>Materiais</h3>
                <div className={styles.news_content}>
                   <News/>
                </div>
@@ -37,8 +37,8 @@ function Dashboard() {
                   <News/>
                </div>
             </div>
-            <div className={`${styles.columnContent} ${styles.square}`}>
-               <h3>Matérias</h3>
+            <div className={`${styles.columnContentPost} ${styles.square}`}>
+               <h3>Posts</h3>
                <FormControl sx={{ m: 1, minWidth: 120}}>
                   <Select
                      sx={{height: 30, borderRadius: 20}}
@@ -61,7 +61,7 @@ function Dashboard() {
                <Card name="Banco de Dados 1" description="Nam volutpat, risus a lacinia fringilla, lectus velit rutrum ipsum, vitae varius elit odio a turpis. Etiam non sem sit amet ante euismod mollis eu eget velit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum varius mauris sit amet risus sollicitudin scelerisque.."/>
             </div>
             <div className={`${styles.columnContent} ${styles.square}`}>
-               <h3>Minhas tarefas</h3>
+               <h3>Monitoria</h3>
             </div>
          </section>
          <Footer />
