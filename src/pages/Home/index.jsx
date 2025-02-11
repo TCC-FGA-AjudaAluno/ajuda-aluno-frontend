96a8c755aa8aa910f8b20fdfb4ff6c067b7f6177
MatheusFilipe95 <mafieu666@hotmail.com>
2025-02-11 20:02:22 -0300

    correção token de login

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -103,7 +103,7 @@ function Home() {
          console.log('onSubmit values: ', values);
          setUsername(values.username)
 
-         let loginPromise = verifyPassword({ username : values.username , password : values.password })
+         let loginPromise = verifyPassword(localStorage.getItem('token'))
          toast.promise(loginPromise, {
          loading: 'Checking...',
          success : <b>Login Successfully...!</b>,
@@ -112,6 +112,7 @@ function Home() {
 
          loginPromise.then(res => {
             let { token, user } = res.data;
+            console.log('RES TOKEN: ', token);
             console.log("Limpando localStorage");
             localStorage.clear();
             localStorage.setItem('token', token);
