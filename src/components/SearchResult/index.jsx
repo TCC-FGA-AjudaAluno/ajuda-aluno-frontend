918def7290bd43644f82cd0e3e5206a8bf13a138
MatheusFilipe95 <mafieu666@hotmail.com>
2025-02-12 16:24:32 -0300

    link dos resultados da barra de pesquisa

--- src/components/SearchResult/index.jsx
+++ src/components/SearchResult/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './SearchResult.module.css';
+import { useNavigate } from 'react-router-dom';
 
 function SearchResult({ result }){
+   const navigate = useNavigate();
+
+   const handleClick = (event) => {
+      navigate(`/subject/${result.id}`);
+    };
    
    return (
       <div 
          className={styles.searchResult} 
-         onClick={(e) => alert(`You clicked on ${result.name}`)}
+         onClick={handleClick}
       >
          { result.name }
       </div>
