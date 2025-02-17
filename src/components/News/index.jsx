21702e99936f24203d3d07446108eca991756736
MatheusFilipe95 <mafieu666@hotmail.com>
2025-02-17 01:52:45 -0300

    Criar e finalizar tarefas

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -3,9 +3,9 @@ import styles from './News.module.css'
 function News(props) {
    return (
       <div className={styles.card + ' ' + styles.box_down + ' ' + styles.orange}>
-            <p style={{margin: "0px"}}><a href={props.href} target="_blank">{props.title}</a></p> 
-            <img src="https://assets.codepen.io/2301174/icon-karma.svg" alt=""/> 
-            <div className={styles.outline}/>
+         <p style={{margin: "0px"}}><a href={props.href} target="_blank">{props.title}</a></p> 
+         <img src="https://assets.codepen.io/2301174/icon-karma.svg" alt=""/> 
+         <div className={styles.outline}/>
       </div>
    )
 }
