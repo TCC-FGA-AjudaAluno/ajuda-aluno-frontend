d5ae2ab427c629b1f0c806a01a368070bdd8aed4
MatheusFilipe95 <mafieu666@hotmail.com>
2025-02-19 19:24:20 -0300

    Ação de like

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -4,54 +4,65 @@ import { BsChat } from "react-icons/bs";
 import styles from './Post.module.css'
 import { Link } from 'react-router-dom';
 import { Button } from '@mui/material';
+import { downvotePost, upvotePost } from '../../helper/helper';
 
 function Post(props) {
-
-   const [color, setColor] = React.useState("transparent");
-   const [textColor, setTextColor] = React.useState("#1976d2");
-
+   console.log("Post props: ", props);
+ 
   const handleLikeClick = () => {
-    setColor(color === "transparent" ? "#1976d2" : "transparent");
-    setTextColor(textColor === "#1976d2" ? "white" : "#1976d2");
+      if(props.post.vote === null || props.post.vote === "DOWNVOTE"){
+         upvotePost(props.post.id).then((res) => {
+            if(res.data){
+               props.updatePosts();
+            } 
+         });
+      }else{
+         //downvote rota
+         downvotePost(props.post.id).then((res) => {
+            if(res.data){
+               props.updatePosts();
+            } 
+         });
+      }
   };
 
    return (
-         <div className={styles.post_card}> 
-            <div className={styles.card + ' ' + styles.box_down + ' ' + styles.blue}> 
+      <div className={styles.post_card}> 
+         <div className={styles.card + ' ' + styles.box_down + ' ' + styles.blue}> 
+            <Link to={`/post/${props.post.id}`} style={{ textDecoration: 'none' }}>
+               <div className={styles.card_title}>
+                  <p style={{margin: "0px 5px 0px 0px", cursor: "pointer"}}>{props.post.title}</p>   
+               </div>
+               <div className={styles.card_description}>
+                  <p className={styles.post_text}>{props.post.content}</p>
+               </div>
+            </Link>
+            <div style={{marginLeft: "1.5em"}}>
+               <Button onClick={handleLikeClick} style={props.post.vote === "UPVOTE" ? {backgroundColor: '#1976d2', color: "white"} : {backgroundColor: "white", color: "#1976d2"}}>
+                  <span className={styles.comment_btnIcons}>
+                     <span style={{margin: "5px 5px 0px 0px"}}>
+                        <BiLike />
+                     </span>
+                     <span>
+                        {props.post.upvotes}
+                     </span>
+                  </span>
+               </Button>
                <Link to={`/post/${props.id}`} style={{ textDecoration: 'none' }}>
-                  <div className={styles.card_title}>
-                     <p style={{margin: "0px 5px 0px 0px", cursor: "pointer"}}>{props.name}</p>   
-                  </div>
-                  <div className={styles.card_description}>
-                     <p className={styles.post_text}>{props.description}</p>
-                  </div>
-               </Link>
-               <div style={{marginLeft: "1.5em"}}>
-                  <Button onClick={handleLikeClick} style={{backgroundColor: color, color: textColor}}>
+                  <Button>
                      <span className={styles.comment_btnIcons}>
                         <span style={{margin: "5px 5px 0px 0px"}}>
-                           <BiLike />
+                           <BsChat />
                         </span>
                         <span>
-                           {props.comments}
+                           {props.post.comments}
                         </span>
                      </span>
                   </Button>
-                  <Link to={`/post/${props.id}`} style={{ textDecoration: 'none' }}>
-                     <Button>
-                        <span className={styles.comment_btnIcons}>
-                           <span style={{margin: "5px 5px 0px 0px"}}>
-                              <BsChat />
-                           </span>
-                           <span>
-                              {props.comments}
-                           </span>
-                        </span>
-                     </Button>
-                  </Link>
-               </div>
+               </Link>
             </div>
          </div>
+      </div>
    )
 }
 
